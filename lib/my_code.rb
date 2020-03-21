d331c9cd2999cd312c08baf2dd67ec9562c28b9c
ramon.l.echeverria <ramon.l.echeverria@gmail.com>
2020-03-21 15:34:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,15 @@
 # My Code here....
+
+# - `map_to_no_change(source_array)`
+# - `map_to_double(source_array)`
+# - `map_to_square(source_array)
+
+def map_to_negativize(source_array)
+  negativize = []
+
+  source_array.each do |num|
+    negative = num * -1
+    negativize << negative
+
+    negativize
+end
